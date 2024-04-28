8de322ebed0f60e97ebf0d142348d2a0bb508f0c
JoachimAA <joachimarthey@hotmail.co.uk>
2024-04-28 13:43:45 +0100

    Updates

---
+++ 4577d2ec-489cdbf0fd88ee2be7ad.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkiamjoachim=self.webpackChunkiamjoachim||[]).push([[817],{1121:function(t,a,c){c.d(a,{OKt:function(){return i}});var h=c(3621);function i(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm7 7v-5h4v5h-4zm2-15.586 6 6V15l.001 5H16v-5c0-1.103-.897-2-2-2h-4c-1.103 0-2 .897-2 2v5H6v-9.586l6-6z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=4577d2ec-489cdbf0fd88ee2be7ad.js.map
